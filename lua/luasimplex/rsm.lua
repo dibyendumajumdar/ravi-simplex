30aa18653e535c41ad5b804ee9b38cf6a0c934ee
dibyendumajumdar <mobile@majumdar.org.uk>
2021-07-11 20:59:28 +0100

    Remove the type assertions

--- lua/luasimplex/rsm.lua
+++ lua/luasimplex/rsm.lua
@@ -17,8 +17,8 @@ local BASIC: integer = 0
 
 local function compute_pi(M: table, I: table)
   -- pi = basic_costs' * Binverse
-  local nrows: integer, pi: number[], Bi: number[], TOL: number = @integer( M.nrows ), @number[]( I.pi ), @number[]( I.Binverse ), @number( I.TOLERANCE )
-  local basic_costs: number[] = @number[]( I.basic_costs )
+  local nrows: integer, pi: number[], Bi: number[], TOL: number = M.nrows, I.pi, I.Binverse, I.TOLERANCE
+  local basic_costs: number[] = I.basic_costs
   for i = 1, nrows do pi[i] = 0 end
   for i = 1, nrows do
     local c: number = basic_costs[i]
@@ -33,12 +33,12 @@ end
 
 local function compute_reduced_cost(M: table, I: table)
   -- reduced cost = cost - pi' * A 
-  local reduced_costs: number[], status: integer[], TOL: number = @number[]( I.reduced_costs ), @integer[]( I.status ), @number( I.TOLERANCE )
-  local indexes: integer[], elements: number[], row_starts: integer[] = @integer[]( M.indexes ), @number[]( M.elements ), @integer[]( M.row_starts )
-  local pi: number[] = @number[]( I.pi )
-  local nvars: integer = @integer( M.nvars )
-  local nrows: integer = @integer( M.nrows )
-  local costs: number[] = @number[]( I.costs )
+  local reduced_costs: number[], status: integer[], TOL: number = I.reduced_costs, I.status, I.TOLERANCE
+  local indexes: integer[], elements: number[], row_starts: integer[] = M.indexes, M.elements, M.row_starts
+  local pi: number[] = I.pi
+  local nvars: integer = M.nvars
+  local nrows: integer = M.nrows
+  local costs: number[] = I.costs
 
   -- initialise with costs (phase 2) or zero (phase 1 and basic variables)
   for i = 1, nvars do
@@ -62,19 +62,19 @@ end
 
 
 local function find_entering_variable(M: table, I: table)
-  local TOL: number = -@number(I.TOLERANCE)
+  local TOL: number = -I.TOLERANCE
   -- Find the variable with the "lowest" reduced cost, keeping in mind that it might be at its upper bound
 
-  local cycles: integer, minrc: number, entering_index: integer = @integer( math.maxinteger ), 0.0, -1
-  local nvars: integer = @integer( M.nvars )
-  local status: integer[] = @integer[]( I.status ) 
-  local reduced_costs: number[] = @number[]( I.reduced_costs )
-  local basic_cycles: integer[] = @integer[]( I.basic_cycles )
+  local cycles: integer, minrc: number, entering_index: integer = math.maxinteger, 0.0, -1
+  local nvars: integer = M.nvars
+  local status: integer[] = I.status 
+  local reduced_costs: number[] = I.reduced_costs
+  local basic_cycles: integer[] = I.basic_cycles
 
   for i = 1, nvars do
     local s: integer, rc: number = status[i]
     if s == NONBASIC_FREE then
-      rc = - @number( abs(reduced_costs[i]) )
+      rc = -abs(reduced_costs[i])
     else
       rc = s * reduced_costs[i]
     end
@@ -91,12 +91,12 @@ end
 
 local function compute_gradient(M: table, I: table, entering_index: integer, g)
   -- gradient = Binverse * entering column of A
-  local nrows: integer, Bi: number[] = @integer( M.nrows ), @number[]( I.Binverse )
-  local indexes: integer[], elements: number[], row_starts: integer[] = @integer[]( M.indexes ), @number[]( M.elements ), @integer[]( M.row_starts )
+  local nrows: integer, Bi: number[] = M.nrows, I.Binverse
+  local indexes: integer[], elements: number[], row_starts: integer[] = M.indexes, M.elements, M.row_starts
   local gradient: number[] = {}
 
   if g then
-    gradient = @number[]( g )
+    gradient = g
     for i = 1, nrows do gradient[i] = 0 end
   else
     gradient = table.numarray(nrows, 0)
@@ -126,14 +126,14 @@ end
 
 
 local function find_leaving_variable(M: table, I: table, entering_index: integer, gradient: number[])
-  local TOL: number = @number( I.TOLERANCE )
-  local status: integer[] = @integer[]( I.status )
-  local reduced_costs: number[] = @number[]( I.reduced_costs )
-  local xu: number[] = @number[]( I.xu )
-  local xl: number[] = @number[]( I.xl )
-  local x: number[] = @number[]( I.x )
-  local nrows: integer, nvars: integer = @integer( M.nrows ), @integer( M.nvars )
-  local basics: integer[] = @integer[]( I.basics )
+  local TOL: number = I.TOLERANCE
+  local status: integer[] = I.status
+  local reduced_costs: number[] = I.reduced_costs
+  local xu: number[] = I.xu
+  local xl: number[] = I.xl
+  local x: number[] = I.x
+  local nrows: integer, nvars: integer = M.nrows, M.nvars
+  local basics: integer[] = I.basics
 
   local s: integer = status[entering_index]
   if s == NONBASIC_FREE then
@@ -177,11 +177,11 @@ end
 
 
 local function update_variables(M: table, I: table)
-  local c: number = @number( I.max_change )
-  local basics: integer[] = @integer[]( I.basics )
-  local x: number[] = @number[]( I.x )
-  local gradient: number[] = @number[]( I.gradient )  
-  local nrows: integer = @integer( M.nrows )
+  local c: number = I.max_change
+  local basics: integer[] = I.basics
+  local x: number[] = I.x
+  local gradient: number[] = I.gradient  
+  local nrows: integer = M.nrows
 
   for i = 1, nrows do
     local j: integer = basics[i]
@@ -191,8 +191,8 @@ end
 
 
 local function update_Binverse(M: table, I: table)
-  local nrows: integer, li: integer, Bi: number[] = @integer( M.nrows ), @integer( I.leaving_index ), @number[]( I.Binverse )
-  local gradient: number[] = @number[]( I.gradient )  
+  local nrows: integer, li: integer, Bi: number[] = M.nrows, I.leaving_index, I.Binverse
+  local gradient: number[] = I.gradient  
 
   local ilg: number = 1.0 / gradient[li]
   for i = 1, nrows do
@@ -212,13 +212,13 @@ end
 -- Initialisation --------------------------------------------------------------
 
 local function initialise_real_variables(M: table, I: table, offset: integer)
-  local nvars: integer = @integer( M.nvars )
-  local I_xu: number[] = @number[]( I.xu )
-  local I_xl: number[] = @number[]( I.xl )
-  local I_x: number[] = @number[]( I.x )
-  local M_xu: number[] = @number[]( M.xu )
-  local M_xl: number[] = @number[]( M.xl )
-  local status: integer[] = @integer[]( I.status )
+  local nvars: integer = M.nvars
+  local I_xu: number[] = I.xu
+  local I_xl: number[] = I.xl
+  local I_x: number[] = I.x
+  local M_xu: number[] = M.xu
+  local M_xl: number[] = M.xl
+  local status: integer[] = I.status
 
   for ii = 1, nvars do
     local i: integer = ii + offset
@@ -238,15 +238,15 @@ end
 
 
 local function initialise_artificial_variables(M: table, I: table, offset: integer)
-  local nrows: integer, nvars: integer = @integer( M.nrows ), @integer( M.nvars )
-  local indexes: integer[], elements: number[], row_starts: integer[] = @integer[]( M.indexes ), @number[]( M.elements ), @integer[]( M.row_starts )
-  local b: number[] = @number[]( M.b )
-  local xu: number[] = @number[]( I.xu )
-  local xl: number[] = @number[]( I.xl )
-  local x: number[] = @number[]( I.x )
-  local basic_costs: number[] = @number[]( I.basic_costs )
-  local basics: integer[] = @integer[]( I.basics )
-  local status: integer[] = @integer[]( I.status )
+  local nrows: integer, nvars: integer = M.nrows, M.nvars
+  local indexes: integer[], elements: number[], row_starts: integer[] = M.indexes, M.elements, M.row_starts
+  local b: number[] = M.b
+  local xu: number[] = I.xu
+  local xl: number[] = I.xl
+  local x: number[] = I.x
+  local basic_costs: number[] = I.basic_costs
+  local basics: integer[] = I.basics
+  local status: integer[] = I.status
 
   for ii = 1, nrows do
     local i: integer = ii + offset
@@ -273,7 +273,7 @@ end
 local function initialise(M: table, I: table, S, c_arrays)
   local offset: integer = c_arrays and -1 or 0
 
-  local nrows: integer = @integer( M.nrows )
+  local nrows: integer = M.nrows
 
   if not S.TOLERANCE then S.TOLERANCE = TOLERANCE end
   I.TOLERANCE = S.TOLERANCE
@@ -281,7 +281,7 @@ local function initialise(M: table, I: table, S, c_arrays)
   initialise_real_variables(M, I, offset)
   initialise_artificial_variables(M, I, offset)
 
-  local Binverse: number[] = @number[]( I.Binverse )
+  local Binverse: number[] = I.Binverse
   for i = 1, nrows do Binverse[(i-1)*nrows + i + offset] = 1 end
 
   return I
@@ -291,17 +291,17 @@ end
 -- Solve -----------------------------------------------------------------------
 
 local function solve(M: table, I: table, S)
-  local TOLERANCE: number = @number( I.TOLERANCE )
-  local x: number[] = @number[]( I.x )
-  local basic_costs: number[] = @number[]( I.basic_costs )
-  local basics: integer[] = @integer[]( I.basics )
-  local status: integer[] = @integer[]( I.status )
-  local x: number[] = @number[]( I.x )
-  local xu: number[] = @number[]( I.xu )
-  local xl: number[] = @number[]( I.xl )
-  local basic_cycles: integer[] = @integer[]( I.basic_cycles )
+  local TOLERANCE: number = I.TOLERANCE
+  local x: number[] = I.x
+  local basic_costs: number[] = I.basic_costs
+  local basics: integer[] = I.basics
+  local status: integer[] = I.status
+  local x: number[] = I.x
+  local xu: number[] = I.xu
+  local xl: number[] = I.xl
+  local basic_cycles: integer[] = I.basic_cycles
 
-  local nvars: integer, nrows: integer = @integer( M.nvars ), @integer( M.nrows )
+  local nvars: integer, nrows: integer = M.nvars, M.nrows
   I.iterations = 0
   I.phase = 1
   local monitor = S.monitor
@@ -336,18 +336,18 @@ local function solve(M: table, I: table, S)
         break  -- optimal
       end
     else
+      local entering_index: integer = I.entering_index
 
-      basic_cycles[I.entering_index] = basic_cycles[I.entering_index] + 1
+      basic_cycles[entering_index] = basic_cycles[entering_index] + 1
 
-      compute_gradient(M, I, I.entering_index, I.gradient)
+      compute_gradient(M, I, entering_index, I.gradient)
       local to_lower
-      I.leaving_index, I.max_change, to_lower = find_leaving_variable(M, I, I.entering_index, I.gradient)
+      I.leaving_index, I.max_change, to_lower = find_leaving_variable(M, I, entering_index, I.gradient)
       if monitor then monitor(M, I, S, "leaving_variable") end
 
-      local max_change: number = @number( I.max_change )
-      local entering_index: integer = @integer( I.entering_index )
-      local leaving_index: integer = @integer( I.leaving_index )
-      local costs: number[] = @number[]( I.costs )
+      local max_change: number = I.max_change
+      local leaving_index: integer = I.leaving_index
+      local costs: number[] = I.costs
 
       if I.phase == 2 and max_change >= math.huge / 2 then
         luasimplex.error("Unbounded", M, I, S)
